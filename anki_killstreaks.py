955ba6717c75432daf2fabb7dff5ddb6b6fabfcf
James Castiglione <jac241@users.noreply.github.com>
2019-12-23 16:25:04 -0500

    Update anki_killstreaks.py

--- anki_killstreaks.py
+++ anki_killstreaks.py
@@ -12,6 +12,7 @@ License: GNU AGPLv3 or later <https://www.gnu.org/licenses/agpl.html>
 ------
 
 Modifications by jac241 <https://github.com/jac241> for Anki Killstreaks addon
+Copyright: (c) jac241 2019
 """
 
 # IMPORTANT:
