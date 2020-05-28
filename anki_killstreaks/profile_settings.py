422b4d36a561d1df1eda72980e1014f2da621838
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-05-28 17:27:39 -0400

    Updates design of Profile dialog, adds logout page
    Once you log in, we switch the QStackedWidget to the logout page, which
    contains a non-functional for now log out button.

--- anki_killstreaks/profile_settings.py
+++ anki_killstreaks/profile_settings.py
@@ -8,10 +8,17 @@ from queue import Queue
 from functools import partial
 
 
-sra_base_url = "https://ankiachievements.com"
+sra_base_url = "http://localhost:5000"
+
+
+def show_dialog(parent, network_thread):
+    ProfileSettingsDialog(parent, network_thread).exec_()
 
 
 class ProfileSettingsDialog(QDialog):
+    loginPageIndex = 0
+    logoutPageIndex = 1
+
     def __init__(self, parent, network_thread):
         super().__init__(parent)
         self.ui = Ui_ProfileSettingsDialog()
@@ -19,9 +26,9 @@ class ProfileSettingsDialog(QDialog):
 
         self._network_thread = network_thread
 
-        self._connect_verify_login_button()
+        self._connect_login_button()
 
-    def _connect_verify_login_button(self):
+    def _connect_login_button(self):
         self.ui.loginButton.clicked.connect(self._verify_login)
 
     def _verify_login(self):
@@ -32,19 +39,25 @@ class ProfileSettingsDialog(QDialog):
             verify_login_on_remote,
             email,
             password,
-            self._on_successful_login
+            self
         )
         self._network_thread.perform_later(job)
 
-    def _on_successful_login(self, user):
-        self.ui.statusLabel.setText(user["email"])
+    def on_successful_login(self, user_attrs):
+        self._switchToLogoutPage(user_attrs)
+
+    def _switchToLogoutPage(self, user_attrs):
+        self.ui.userEmailLabel.setText(user_attrs["email"])
+        self.ui.stackedWidget.setCurrentIndex(self.logoutPageIndex)
+
+    def on_unauthorized(self, response):
+        self.ui.statusLabel.setText(response["errors"][0])
+
+    def on_connection_error(self):
+        self.ui.statusLabel.setText("Error connecting to server. Try again later.")
 
 
-def show_dialog(parent, network_thread):
-    ProfileSettingsDialog(parent, network_thread).exec_()
-
-
-def verify_login_on_remote(email, password, on_success):
+def verify_login_on_remote(email, password, listener):
     headers={
         "user_email": "JimmyYoshi@gmail.com",
         "Accept": "application/json",
@@ -57,6 +70,17 @@ def verify_login_on_remote(email, password, on_success):
     )
     url = urljoin(sra_base_url, "api/v1/auth/sign_in")
 
-    response = requests.post(url, headers=headers, json=body)
-    print(response.text)
-    on_success(response.json()["data"])
+    try:
+        response = requests.post(url, headers=headers, json=body)
+
+        print(response.status_code)
+        print(response.text)
+
+        if response.status_code == 200:
+            listener.on_successful_login(response.json()["data"])
+        elif response.status_code == 401:
+            listener.on_unauthorized(response.json())
+        else:
+            raise RuntimeError("Unhandled response status", response)
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        listener.on_connection_error()
